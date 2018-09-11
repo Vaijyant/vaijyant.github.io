7091db6fe4db5c467d6f87dce4e437cb75d78aa5
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-09-11 01:43:55 -0400

    centered cheveron

--- resources/js/typeWriter.js
+++ resources/js/typeWriter.js
@@ -1,4 +1,8 @@
 var typed = new Typed('#welcome-message-text', {
-    strings: ["Hi,<br/>I am Vaijyant!"],
-    typeSpeed: 30
+    strings: ["Hi,<br/>I am Vaijyant!<br/>I am a programmer.",
+        "Hi,<br/>I am Vaijyant!<br/>I am an engineer.",
+        "Hi,<br/>I am Vaijyant!<br/>I am a problem solver.",
+        ":)", ":^)", ":P", ":D"],
+    typeSpeed: 90,
+    smartBackspace: true
 });
