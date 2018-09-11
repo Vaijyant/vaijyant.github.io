f229db1c5643df2f58c411314b1045044be9bb3b
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-09-11 02:37:27 -0400

    title card update

--- resources/js/typeWriter.js
+++ resources/js/typeWriter.js
@@ -2,7 +2,7 @@ var typed = new Typed('#welcome-message-text', {
     strings: ["Hi,<br/>I am Vaijyant!<br/>I am a programmer.",
         "Hi,<br/>I am Vaijyant!<br/>I am an engineer.",
         "Hi,<br/>I am Vaijyant!<br/>I am a problem solver.",
-        ":)", ":^)", ":P", ":D"],
+        ":)", ":^)", ":P", ":D^2750"],
     typeSpeed: 90,
-    smartBackspace: true
+    onComplete: (self) => {scrollToMain()}
 });
